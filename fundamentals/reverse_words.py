90972d02a34388c271ae24c0a4c4e412e1d3ae8b
rapampamfau <rafal.szczepanczyk7@gmail.com>
2024-01-03 22:03:51 +0100

    reversing words in text

---
+++ fundamentals/reverse_words.py
@@ -0,0 +1,20 @@
+def reverse_words(text):
+    str_list = text.split(" ")
+    result = []
+
+    for i in range(0, len(str_list)):
+        word_as_list = list(str_list[i])
+        word_as_list.reverse()
+        reversed_word = "".join(word_as_list)
+
+        if i != len(str_list) -1:
+            result.append(reversed_word)
+            result.append(" ")
+        else:
+            result.append(reversed_word)
+    
+    return "".join(result)
+        
+
+
+print(reverse_words("X D XD "))
